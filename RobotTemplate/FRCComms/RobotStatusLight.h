7559b35debc01d498ac3586f581f97f1aebc1abc
Eric Haskins <Eric Haskins@localhost>
2011-02-21 21:02:17 -0600

    Misc config issues. RSL droped packet blink impletmented.

--- RobotTemplate/FRCComms/RobotStatusLight.h
+++ RobotTemplate/FRCComms/RobotStatusLight.h
@@ -12,11 +12,15 @@
 #include "Period.h"
 
 static const int NUM_PERIODS = 4;
+static const unsigned int DROPPED_BLINK_DURATION = 50;
+
 struct RobotStatusLight {
+private:
+	unsigned long droppedStartTime;
+
 public:
 	void init(int);
-	void update(bool connected,bool configMode,  Mode mode);
-
+	void update(bool connected,bool droppedPacket,  Mode mode);
 	int period;
 	bool packetDropped;
 	unsigned long lastStateChangeTime;
